81bfd705ccf06f0d33b548ca7824121e1c8349c7
TvanZelderen <103991671+TvanZelderen@users.noreply.github.com>
2022-11-14 14:50:00 +0100

    Added first two functions from appendix A

---
+++ WP4/wp4_1.py
@@ -0,0 +1,6 @@
+def torque_inboard(torque_outboard, moment_outboard, sweep_angle):
+    return torque_outboard * cos(sweep_angle) + moment_outboard * sin(sweep_angle)
+
+
+def moment_inboard(torque_outboard, moment_outboard, sweep_angle):
+    return moment_outboard * cos(sweep_angle) - torque_outboard * sin(sweep_angle)
