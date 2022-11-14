6e2b219aba4dc1931aa28b5566aebeb08ebbf125
TvanZelderen <103991671+TvanZelderen@users.noreply.github.com>
2022-11-14 15:01:07 +0100

    More functions added

--- WP4/wp4_1.py
+++ WP4/wp4_1.py
@@ -1,3 +1,5 @@
+import scipy as sp
+from scipy import interpolate
 from math import cos, sin
 
 
@@ -7,3 +9,11 @@ def torque_inboard(torque_outboard, moment_outboard, sweep_angle):
 
 def moment_inboard(torque_outboard, moment_outboard, sweep_angle):
     return moment_outboard * cos(sweep_angle) - torque_outboard * sin(sweep_angle)
+
+
+def dynamic_pressure(velocity, density):
+    return 0.5 * density * velocity**2
+
+
+def lift_pu_span(dynamic_pressure, lift_coefficient, chord):
+    return dynamic_pressure * lift_coefficient * chord
