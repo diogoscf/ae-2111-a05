c0b9f86cd537df92145dcb40622bb957ff3d175c
TimTsai1 <118188386+TimTsai1@users.noreply.github.com>
2022-11-14 15:24:48 +0100

    Imports XFLR data

---
+++ WP4/XFLR Importer.py
@@ -0,0 +1,12 @@
+import scipy as sp
+import numpy as np 
+
+XFLRData = np.genfromtxt("MainWing_alpha_zero_v_ten.txt", skip_header=40, skip_footer=1029)
+y_lst = XFLRData[:,0]
+c_lst = XFLRData[:,1]
+cl_lst = XFLRData[:,3]
+cd_lst = XFLRData[:,5]
+cm_lst = XFLRData[:,7]
+print(cm_lst)
+
+def 
