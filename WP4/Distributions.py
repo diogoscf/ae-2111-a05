c8b459e6e0e5cd2b897c04aeb6d0b95e37c77d1a
TimTsai1 <118188386+TimTsai1@users.noreply.github.com>
2022-11-14 16:58:44 +0100

    Why do i need to do this

--- WP4/Distributions.py
+++ WP4/Distributions.py
@@ -1,4 +1,32 @@
 import XFLR
-import ISA_calculator
+#import ISA_calculator
+import matplotlib.pyplot as plt 
 #Use XFLR.interpolater and XFLR.listtype
-Density = ISA_calculator
+Density = 1
+velocity = 10
+dyn_p = 0.5*Density*velocity**2
+
+
+
+
+def L_prime(y_pos):
+    return(XFLR.interpolater(y_pos, XFLR.cl_lst)*dyn_p*XFLR.interpolater(y_pos, XFLR.c_lst))
+
+def M_prime(y_pos):
+    return(XFLR.interpolater(y_pos, XFLR.cm_lst)*dyn_p*XFLR.interpolater(y_pos, XFLR.c_lst)**2)
+
+def D_prime(y_pos):
+    return(XFLR.interpolater(y_pos, XFLR.cd_lst)*dyn_p*XFLR.interpolater(y_pos, XFLR.c_lst))
+i=-22 
+y_lst = []
+D_lst=[]
+L_lst = []
+M_lst = []
+while i<22:
+    D_lst.append(D_prime(i))
+    L_lst.append(L_prime(i))
+    M_lst.append(M_prime(i))
+    y_lst.append(i)
+    i+=0.01
+    
+plt.plot(y_lst, D_lst)
--- WP4/XFLR.py
+++ WP4/XFLR.py
@@ -16,6 +16,7 @@ cd_lst = XFLRData[:,5]
 cm_lst = XFLRData[:,7]
 
 #List interpolater, y_post is wanted y postition, listtype is which value tou want to interpolate: cl_lst, cm_lst etc
+#Use XFLR.listtype in other files. i.e. xflr.cl_list
 def interpolater(y_pos, listtype):
     value = sp.interpolate.interp1d(y_lst, listtype, kind='cubic', fill_value="extrapolate")
     value = value(abs(y_pos))
