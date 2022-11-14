fc8ded03f23c03bbd693cabd0ef7bf3a7a4933f2
TvanZelderen <103991671+TvanZelderen@users.noreply.github.com>
2022-11-14 14:51:00 +0100

    Added cos and sin

--- WP4/wp4_1.py
+++ WP4/wp4_1.py
@@ -1,3 +1,6 @@
+from math import cos, sin
+
+
 def torque_inboard(torque_outboard, moment_outboard, sweep_angle):
     return torque_outboard * cos(sweep_angle) + moment_outboard * sin(sweep_angle)
 
