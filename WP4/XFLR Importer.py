3ff9413b08198733d1ed6d8d35f78781dadfd5cb
TimTsai1 <118188386+TimTsai1@users.noreply.github.com>
2022-11-14 15:30:02 +0100

    Update XFLR Importer.py

--- WP4/XFLR Importer.py
+++ WP4/XFLR Importer.py
@@ -1,7 +1,9 @@
 import scipy as sp
 import numpy as np 
-#Imports data
+#Imports data, change "MainWing_alpha_zero_v_ten.txt" to "FILENAMEHERE" to change files.
+#Check is skip_header and skip_footer are appropriate in the new file. I think it'll be fine but just in case.
 XFLRData = np.genfromtxt("MainWing_alpha_zero_v_ten.txt", skip_header=40, skip_footer=1029)
+#Separates XFLRData into lists
 y_lst = XFLRData[:,0]
 c_lst = XFLRData[:,1]
 cl_lst = XFLRData[:,3]
