3fe32872a90d4ac4c6ac99e3fb41581ee86b4557
Diogo Fernandes <diogo@diogos.cf>
2022-11-16 12:41:12 +0100

    actually fix xflr

--- WP4/XFLR.py
+++ WP4/XFLR.py
@@ -7,11 +7,12 @@ from scipy import interpolate
 
 # Imports data, change "MainWing_alpha_zero_v_ten.txt" to "FILENAMEHERE" to change files.
 # Check is skip_header and skip_footer are appropriate in the new file. I think it'll be fine but just in case.
-XFLR_path = os.path.join(os.path.dirname(__file__), "./MainWing_alpha_zero_v_ten.txt")
+XFLR0_path = os.path.join(os.path.dirname(__file__), "./MainWing_alpha_zero_v_ten.txt")
+XFLR10_path = os.path.join(os.path.dirname(__file__), "./MainWing_alpha_ten_v_ten.txt") 
 XFLRData_0 = np.genfromtxt(
-    XFLR_path, skip_header=40, skip_footer=1029
+    XFLR0_path, skip_header=40, skip_footer=1029
 )
-XFLRData_10 = np.genfromtxt(XFLR_path, skip_header=40, skip_footer=1029)
+XFLRData_10 = np.genfromtxt(XFLR10_path, skip_header=40, skip_footer=1029)
 # Separates XFLRData into lists
 y_lst = XFLRData_0[:, 0]
 c_lst = XFLRData_0[:, 1]
