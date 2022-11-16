3bd4d1e58ac7d645f0ac94b8f1be5a499229a45e
TvanZelderen <103991671+TvanZelderen@users.noreply.github.com>
2022-11-16 15:32:24 +0100

    Updated, and broken

--- WP4/Diagrams.py
+++ WP4/Diagrams.py
@@ -3,7 +3,7 @@ import scipy as sp
 from scipy import integrate
 import Distributions
 import matplotlib.pyplot as plt
-from math import sin, cos
+from math import sin, cos, pi
 
 
 def normal_force_per_span(lift_per, drag_per, angle_of_attack):
@@ -20,12 +20,15 @@ def shear_force_diagram(cl_d):
         y_tab.append(
             normal_force_per_y := normal_force_per_span(
                 Distributions.L_prime(cl_d, y_pos[i], Distributions.dyn_p),
-                Distributions.D_prime(cl_d, y_pos[i], Distributions.dyn_p),
+                Distributions.D_prime(
+                    0.028 + cl_d**2 / (pi * 10 * 0.51), y_pos[i], Distributions.dyn_p
+                ),
                 Distributions.AOA_specific_flight_regime(cl_d),
             )
         )
         x_tab.append(y_pos[i])
 
+    print(y_tab)
     plt.plot(x_tab, y_tab)
     plt.title(f"Shear force at a cl of {cl_d}")
     plt.show()
