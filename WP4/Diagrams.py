28c7fa217c56708e6203f713c55e6c8b7aecdbcc
Diogo Fernandes <diogo@diogos.cf>
2022-12-02 15:18:26 +0100

    better diagrams

--- WP4/Diagrams.py
+++ WP4/Diagrams.py
@@ -45,14 +45,19 @@ def shear_force_diagram(
     distributed_loads=[wing_mass],
     load_factor=1,
     dyn_p=10000,
+    ax=False,
     y_pos=y_space,
 ):
     shear_force = shear_force_calc(cl_d, point_loads, distributed_loads, load_factor, dyn_p, y_pos)
-    plt.plot(y_pos, shear_force)
-    plt.title(f"Shear force at cl: {cl_d} n: {load_factor}, and dyn_p: {round(dyn_p, 1)}")
-    plt.xlabel("y [m]")
-    plt.ylabel("Shear force [N]")
-    plt.show()
+    if ax:
+        ax.plot(y_pos, shear_force, label=f"Cl: {cl_d}, n: {load_factor}, and q_∞: {round(dyn_p, 1)}")
+    else:
+        plt.plot(y_pos, shear_force)
+        plt.title(f"Shear force at cl: {cl_d} n: {load_factor}, and dyn_p: {round(dyn_p, 1)}")
+        plt.xlabel("y [m]")
+        plt.ylabel("Shear force [N]")
+        plt.grid()
+        plt.show()
 
 
 def moment_calc(
@@ -83,14 +88,19 @@ def moment_diagram(
     distributed_loads=[],
     load_factor=1,
     dyn_p=10000,
+    ax=False,
     y_pos=y_space,
 ):
     moments = moment_calc(cl_d, point_loads, distributed_loads, load_factor, dyn_p, y_pos)
-    plt.plot(y_pos, moments)
-    plt.title(f"Moment at cl: {cl_d}, n: {load_factor}, and dyn_p: {round(dyn_p, 1)}")
-    plt.xlabel("y [m]")
-    plt.ylabel("Moment [Nm]")
-    plt.show()
+    if ax:
+        ax.plot(y_pos, moments, label=f"Cl: {cl_d}, n: {load_factor}, and q_∞: {round(dyn_p, 1)}")
+    else:
+        plt.plot(y_pos, moments)
+        plt.title(f"Moment at cl: {cl_d}, n: {load_factor}, and dyn_p: {round(dyn_p, 1)}")
+        plt.xlabel("y [m]")
+        plt.ylabel("Moment [Nm]")
+        plt.grid()
+        plt.show()
 
 
 def distance_flexural_axis(y):
@@ -113,34 +123,54 @@ def torque_calc(cl_d, point_loads=[], load_factor=1, dyn_p=10000, y_pos=y_space)
     return t_tab
 
 
-def torque_diagram(cl_d, point_loads=[], load_factor=1, dyn_p=10000, y_pos=y_space):
+def torque_diagram(cl_d, point_loads=[], load_factor=1, dyn_p=10000, ax=False, y_pos=y_space):
     torque = torque_calc(cl_d, point_loads, load_factor, dyn_p, y_pos)
-    plt.plot(y_pos, torque)
-    plt.title(f"Torque at cl: {cl_d}, n: {load_factor}, and dyn_p: {round(dyn_p, 1)}")
-    plt.xlabel("y [m]")
-    plt.ylabel("Torque [Nm]")
-    plt.show()
-
+    if ax:
+        ax.plot(y_pos, torque, label=f"Cl: {cl_d}, n: {load_factor}, and q_∞: {round(dyn_p, 1)}")
+    else:
+        plt.plot(y_pos, torque)
+        plt.title(f"Torque at cl: {cl_d}, n: {load_factor}, and dyn_p: {round(dyn_p, 1)}")
+        plt.xlabel("y [m]")
+        plt.ylabel("Torque [Nm]")
+        plt.grid()
+        plt.show()
 
 if __name__ == "__main__":
-    shear_force_diagram(0.908, [engine_mass], [wing_mass], 3.75, 3703.338805)
-    shear_force_diagram(0.908, [engine_mass], [wing_mass], 3.75, 8328.245793)
-    shear_force_diagram(0.908, [engine_mass], [wing_mass], 3.75, 4838.801824)
-    shear_force_diagram(2.27, [engine_mass], [wing_mass], -1.5, 1481.335522)
-    shear_force_diagram(2.27, [engine_mass], [wing_mass], -1.5, 3331.298316)
-    shear_force_diagram(2.27, [engine_mass], [wing_mass], -1.5, 1935.52073)
-    moment_diagram(0.908, [engine_mass], [wing_mass], 3.75, 3703.338805)
-    moment_diagram(0.908, [engine_mass], [wing_mass], 3.75, 8328.245793)
-    moment_diagram(0.908, [engine_mass], [wing_mass], 3.75, 4838.801824)
-    moment_diagram(2.27, [engine_mass], [wing_mass], -1.5, 1481.335522)
-    moment_diagram(2.27, [engine_mass], [wing_mass], -1.5, 3331.298316)
-    moment_diagram(2.27, [engine_mass], [wing_mass], -1.5, 1935.52073)
-    torque_diagram(0.908, [engine_thrust], 3.75, 3703.338805)
-    torque_diagram(0.908, [engine_thrust], 3.75, 8328.245793)
-    torque_diagram(0.908, [engine_thrust], 3.75, 4838.801824)
-    torque_diagram(2.27, [], -1.5, 1481.335522)
-    torque_diagram(2.27, [], -1.5, 3331.298316)
-    torque_diagram(2.27, [], -1.5, 1935.52073)
+    fig, ax_shear = plt.subplots()
+    shear_force_diagram(0.908, [engine_mass], [wing_mass], 3.75, 3703.338805, ax_shear)
+    shear_force_diagram(0.908, [engine_mass], [wing_mass], 3.75, 4838.801824, ax_shear)
+    shear_force_diagram(0.908, [engine_mass], [wing_mass], 3.75, 8328.245793, ax_shear)
+    shear_force_diagram(2.27, [engine_mass], [wing_mass], -1.5, 1481.335522, ax_shear)
+    shear_force_diagram(2.27, [engine_mass], [wing_mass], -1.5, 1935.52073, ax_shear)
+    shear_force_diagram(2.27, [engine_mass], [wing_mass], -1.5, 3331.298316, ax_shear)
+    ax_shear.set(title="Shear Force Diagram", xlabel="y [m]", ylabel="Shear force [N]")
+    ax_shear.legend()
+    ax_shear.grid()
+
+    fig, ax_moment = plt.subplots()
+    moment_diagram(0.908, [engine_mass], [wing_mass], 3.75, 3703.338805, ax_moment)
+    moment_diagram(0.908, [engine_mass], [wing_mass], 3.75, 4838.801824, ax_moment)
+    moment_diagram(0.908, [engine_mass], [wing_mass], 3.75, 8328.245793, ax_moment)
+    moment_diagram(2.27, [engine_mass], [wing_mass], -1.5, 1481.335522, ax_moment)
+    moment_diagram(2.27, [engine_mass], [wing_mass], -1.5, 1935.52073, ax_moment)
+    moment_diagram(2.27, [engine_mass], [wing_mass], -1.5, 3331.298316, ax_moment)
+    ax_moment.set(title="Moment Diagram", xlabel="y [m]", ylabel="Moment [Nm]")
+    ax_moment.legend()
+    ax_moment.grid()
+
+    fig, ax_torque = plt.subplots()
+    torque_diagram(0.908, [engine_thrust], 3.75, 3703.338805, ax_torque)
+    torque_diagram(0.908, [engine_thrust], 3.75, 4838.801824, ax_torque)
+    torque_diagram(0.908, [engine_thrust], 3.75, 8328.245793, ax_torque)
+    torque_diagram(2.27, [], -1.5, 1481.335522, ax_torque)
+    torque_diagram(2.27, [], -1.5, 1935.52073, ax_torque)
+    torque_diagram(2.27, [], -1.5, 3331.298316, ax_torque)
+    ax_torque.set(title="Torque Diagram", xlabel="y [m]", ylabel="Torque [Nm]")
+    ax_torque.legend()
+    ax_torque.grid()
+
+    plt.show()
+
     # print(shear_force_calc(0.908, [engine_mass], [wing_mass], 3.75, 3703.338805)[0])
     # print(shear_force_calc(0.908, [engine_mass], [wing_mass], 3.75, 8328.245793)[0])
     # print(shear_force_calc(0.908, [engine_mass], [wing_mass], 3.75, 4838.801824)[0])
