40f4a07f7ef940e421062d5ed4ac90d8243262bf
Diogo Fernandes <diogo@diogos.cf>
2022-12-12 13:51:16 +0100

    param change

--- WP4/params.py
+++ WP4/params.py
@@ -11,20 +11,20 @@ WINGBOX = {
     "front_spar": 0.2,
     "rear_spar": 0.65,
     "other_spars": [
-        (0.25, 0.4),
-        (0.3, 1),
+        #(0.25, 0.4),
+        (0.3, 0.4),
         #(0.35, 0.4),
         (0.4, 1),
         #(0.45, 0.4),
-        (0.5, 1),
-        (0.55, 0.4),
-        (0.6, 1),
+        (0.5, 0.4),
+        #(0.55, 0.4),
+        #(0.6, 1),
     ],  # (x/c position value, y/(b/2) value of end of spar)
     "spar_thickness": (25e-3, 10e-3),  # (t_root, t_tip) interpolated linearly. [meter]
-    "skin_thickness": 10e-3,  # meter
-    "stringer_area": 480e-6,  # square meter
-    "stringers_top": [(50, 0.4), (40, 0.65), (20, 1)],  # *Ordered* List: (nstringers, y/(b/2) value of end) - start is end of previous or root
-    "stringers_bottom": [(50, 0.4), (40, 0.65), (20, 1)]  # same as above
+    "skin_thickness": 21e-3,  # meter
+    "stringer_area": 80e-6,  # square meter
+    "stringers_top": [(10, 0.4), (10, 0.65), (10, 1)],  # *Ordered* List: (nstringers, y/(b/2) value of end) - start is end of previous or root
+    "stringers_bottom": [(10, 0.4), (10, 0.65), (10, 1)]  # same as above
 }
 
 # Wingbox Material Parameters (in SI)
