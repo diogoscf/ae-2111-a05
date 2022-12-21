5cd0e74c19d7a70192344767eb4a8dad6325559e
oliksi <O.Ksiazek@student.tudelft.nl>
2022-12-21 20:01:02 +0100

    Update column_buckling.py

--- WP4_5/column_buckling.py
+++ WP4_5/column_buckling.py
@@ -7,9 +7,9 @@ from params import *
 from compression_failure import *
 from design_options import *
 
-#numbers below are arbitrary
 STRINGER = {
     "vertical_l": 60, #mm
+    "horizontal_l": 60, #mm
     "thickness": 6, #mm
     "E_AL6061-T6": 68.9*10**9 #Pa
 }
@@ -18,7 +18,7 @@ STRINGER = {
 option=WINGBOX #current design
 
 #area of one stringer
-vl=STRINGER["vertical_l"]
+vl,hl=STRINGER["vertical_l"],STRINGER["horizontal_l"]
 t=STRINGER["thickness"]
 A=vl**2/5
 #print('stringer area:',A, 'mm^2')
@@ -28,12 +28,13 @@ Ribs_pos=WINGBOX["ribs"]# -> 0.4 and 0.65
 #Ribs_pos=(0,0.05,0.1,0.15,0.21,0.27,0.33,0.4,0.47,0.56,0.65,0.79,1)
 Index=range(0,len(Ribs_pos))
 
-def Ixx(t,Lv): #of one stringer
+def Ixx(t,Lv,Lh): #of one stringer
     t=float(t)
+    Lh=float(Lh)
     Lv=float(Lv)
-    y=(0.5*Lv*Lv*t)/(Lv*t+Lv*t)
+    y=(0.5*Lv*Lv*t)/(Lh*t+Lv*t)
 
-    Ixx= Lv*t*y**2 + 1/12*t*Lv**3 + Lv*t*(0.5*Lv-y)**2
+    Ixx= Lh*t*y**2 + 1/12*t*Lv**3 + Lv*t*(0.5*Lv-y)**2
     return Ixx
 
 #o crit of segment @ y/(b/2) for chosen design
@@ -43,7 +44,7 @@ def crit_buckling_str(y):
     L= (Ribs_pos[int(g(y))]-Ribs_pos[int(g(y)-1)])*(WING["span"]*1000)/2 #mm; unsupported length
     E= STRINGER["E_AL6061-T6"]
     K=4 #assume rib at the very end
-    I=Ixx(t,vl) 
+    I=Ixx(t,vl,hl) 
 
     o_cr= (K*pi**2*E*I)/(L**2*A) *10**-6 #MPa
     return o_cr
